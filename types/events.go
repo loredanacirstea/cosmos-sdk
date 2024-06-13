2bf8b71ec68f553d62b25d29c7873fd9ad872d69
Loredana Cirstea <loredana.cirstea@gmail.com>
2024-06-13 22:46:33 +0200

    make cosmos-sdk events deterministic

--- types/events.go
+++ types/events.go
@@ -316,7 +316,8 @@ func StringifyEvents(events []abci.Event) StringEvents {
 // MarkEventsToIndex returns the set of ABCI events, where each event's attribute
 // has it's index value marked based on the provided set of events to index.
 func MarkEventsToIndex(events []abci.Event, indexSet map[string]struct{}) []abci.Event {
-	indexAll := len(indexSet) == 0
+	// remove nondeterminism
+	// indexAll := len(indexSet) == 0
 	updatedEvents := make([]abci.Event, len(events))
 
 	for i, e := range events {
@@ -326,11 +327,11 @@ func MarkEventsToIndex(events []abci.Event, indexSet map[string]struct{}) []abci
 		}
 
 		for j, attr := range e.Attributes {
-			_, index := indexSet[fmt.Sprintf("%s.%s", e.Type, attr.Key)]
+			// _, index := indexSet[fmt.Sprintf("%s.%s", e.Type, attr.Key)]
 			updatedAttr := abci.EventAttribute{
 				Key:   attr.Key,
 				Value: attr.Value,
-				Index: index || indexAll,
+				Index: attr.Index,
 			}
 
 			updatedEvent.Attributes[j] = updatedAttr
