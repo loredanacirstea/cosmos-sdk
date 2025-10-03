449957441ffa7eded7f3155b82dcec6f11d65edc
Loredana Cirstea <loredana.cirstea@gmail.com>
2025-10-03 15:06:22 +0200

    tmp

--- store/gaskv/store.go
+++ store/gaskv/store.go
@@ -41,7 +41,7 @@ func (gs *Store) Get(key []byte) (value []byte) {
 	// TODO overflow-safe math?
 	gs.gasMeter.ConsumeGas(gs.gasConfig.ReadCostPerByte*types.Gas(len(key)), types.GasReadPerByteDesc)
 	gs.gasMeter.ConsumeGas(gs.gasConfig.ReadCostPerByte*types.Gas(len(value)), types.GasReadPerByteDesc)
-	fmt.Println("*gaskv.get--", hex.EncodeToString(key), string(key))
+	fmt.Println("*gaskv.get--", gs.parent.GetStoreType(), hex.EncodeToString(key), string(key))
 
 	return value
 }
@@ -55,13 +55,13 @@ func (gs *Store) Set(key, value []byte) {
 	gs.gasMeter.ConsumeGas(gs.gasConfig.WriteCostPerByte*types.Gas(len(key)), types.GasWritePerByteDesc)
 	gs.gasMeter.ConsumeGas(gs.gasConfig.WriteCostPerByte*types.Gas(len(value)), types.GasWritePerByteDesc)
 	gs.parent.Set(key, value)
-	fmt.Println("*gaskv.set--", hex.EncodeToString(key), string(key), hex.EncodeToString(value), string(value))
+	fmt.Println("*gaskv.set--", gs.parent.GetStoreType(), hex.EncodeToString(key), string(key), hex.EncodeToString(value), string(value))
 }
 
 // Implements KVStore.
 func (gs *Store) Has(key []byte) bool {
 	gs.gasMeter.ConsumeGas(gs.gasConfig.HasCost, types.GasHasDesc)
-	fmt.Println("*gaskv.has--", hex.EncodeToString(key), string(key))
+	fmt.Println("*gaskv.has--", gs.parent.GetStoreType(), hex.EncodeToString(key), string(key))
 	return gs.parent.Has(key)
 }
 
@@ -70,7 +70,7 @@ func (gs *Store) Delete(key []byte) {
 	// charge gas to prevent certain attack vectors even though space is being freed
 	gs.gasMeter.ConsumeGas(gs.gasConfig.DeleteCost, types.GasDeleteDesc)
 	gs.parent.Delete(key)
-	fmt.Println("*gaskv.delete--", hex.EncodeToString(key), string(key))
+	fmt.Println("*gaskv.delete--", gs.parent.GetStoreType(), hex.EncodeToString(key), string(key))
 }
 
 // Iterator implements the KVStore interface. It returns an iterator which
